a462832deb2206944917daf874778cce1f611552
Otavio Fernandes <otavio@neton.com.br>
2013-10-24 23:04:43 -0200

    Fix

--- src/android/com/neton/Diagnostic.java
+++ src/android/com/neton/Diagnostic.java
@@ -38,17 +38,7 @@ public class Diagnostic extends CordovaPlugin {
             switchToLocationSettings(data, callbackContext);
         } else if ("isGpsEnabled".equals(action))
             isGpsEnabled(data, callbackContext);
-        else if ("isWirelessNetworkLocationEnabled".equals(action))
-            isWirelessNetworkLocationEnabled(data, callbackContext);
-        else if ("isWifiEnabled".equals(action))
-            isWifiEnabled(data, callbackContext);
-        else if ("switchToWifiSettings".equals(action)) {
-            switchToWifiSettings(data, callbackContext);
-        } else if ("isBluetoothEnabled".equals(action))
-            isBluetoothEnabled(data, callbackContext);
-        else if ("switchToBluetoothSettings".equals(action)) {
-            switchToBluetoothSettings(data, callbackContext);
-        } else {
+        else {
             Log.d(LOG_TAG, "Invalid action");
             callbackContext.error("Invalid action");
 
@@ -99,82 +89,5 @@ public class Diagnostic extends CordovaPlugin {
 
     }
 
-    /**
-     * Check device settings for wireless network location (Wi-Fi and/or mobile
-     * networks).
-     * 
-     * @returns {boolean} The status of wireless network location in device
-     *          settings.
-     */
-    public void isWirelessNetworkLocationEnabled(JSONArray data, CallbackContext callbackContext) throws JSONException {
-        boolean result = isLocationProviderEnabled(LocationManager.NETWORK_PROVIDER);
-        Log.d(LOG_TAG, "Wireless Network Location enabled: " + result);
 
-        if (result)
-            callbackContext.success("true");
-        else
-            callbackContext.success("false");
-
-    }
-
-    private void isLocationProviderEnabled(JSONArray data, CallbackContext callbackContext) throws JSONException {
-        //LocationManager locationManager = (LocationManager) ctx.getSystemService(Context.LOCATION_SERVICE);
-        //return locationManager.isProviderEnabled(provider);
-        callbackContext.success();
-    }
-
-    /**
-     * Check device settings for Wi-Fi.
-     * 
-     * @returns {boolean} The status of Wi-Fi in device settings.
-     */
-    public void isWifiEnabled(JSONArray data, CallbackContext callbackContext) throws JSONException {
-        WifiManager wifiManager = (WifiManager) ctx.getSystemService(Context.WIFI_SERVICE);
-        boolean result = wifiManager.isWifiEnabled();
-        Log.d(LOG_TAG, "Wifi enabled: " + result);
-
-        if (result)
-            callbackContext.success("true");
-        else
-            callbackContext.success("false");
-
-    }
-    
-    /**
-     * Requests that the user enable the Wi-Fi in device settings.
-     */
-    public void switchToWifiSettings(JSONArray data, CallbackContext callbackContext) throws JSONException {
-        Log.d(LOG_TAG, "Switch to Wifi Settings");
-        Intent settingsIntent = new Intent(Settings.ACTION_WIFI_SETTINGS);
-        ctx.startActivity(settingsIntent);
-        callbackContext.success();
-    }
-    
-    /**
-     * Check device settings for Bluetooth.
-     * 
-     * @returns {boolean} The status of Bluetooth in device settings.
-     */
-    public void isBluetoothEnabled(JSONArray data, CallbackContext callbackContext) throws JSONException {
-        Looper.prepare();
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        boolean result = bluetoothAdapter.isEnabled();
-        Log.d(LOG_TAG, "Bluetooth enabled: " + result);
-
-        if (result)
-            callbackContext.success("true");
-        else
-            callbackContext.success("false");
-
-    }
-    
-    /**
-     * Requests that the user enable the Bluetooth in device settings.
-     */
-    public void switchToBluetoothSettings(JSONArray data, CallbackContext callbackContext) throws JSONException {
-        Log.d(LOG_TAG, "Switch to Bluetooth Settings");
-        Intent settingsIntent = new Intent(Settings.ACTION_BLUETOOTH_SETTINGS);
-        ctx.startActivity(settingsIntent);
-        callbackContext.success();
-    }
 }
